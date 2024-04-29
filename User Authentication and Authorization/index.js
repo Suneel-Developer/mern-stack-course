ed1bd86a57f86a4e5f9e08bace9150b318c9f0a2
suneeldeveloper <suneeldeveloper11@gmail.com>
2024-04-29 07:50:20 -0700

    connect to frontend & make a Register, login & get user data

--- User Authentication and Authorization/index.js
+++ User Authentication and Authorization/index.js
@@ -14,12 +14,10 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
 
+
 function autnenticateToken(req, res, next) {
   const token = req.headers.authorization.split(" ")[1];
-  //   const token = req.headers.authorization;
-  const { id } = req.body;
 
-  //   if (!token) return res.status(401).json({ message: "Auth Error" });
   if (!token) {
     const error = new Error("Token Not Found");
     next(error);
@@ -27,25 +25,21 @@ function autnenticateToken(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    if (id && decoded.id !== id) {
-      const error = new Error("Invalid Token");
-      next(error);
-    }
-    req.id = decoded;
-    next();
+
+    const userid = decoded.id;
+    req.id = userid
+    next()
   } catch (err) {
-    //     console.log(err);
-    //     res.status(500).json({ message: "Invalid Token" });
     next(err);
   }
 }
 
 app.get("/", (req, res) => {
-  res.send("API IS WORKING");
+  res.json({ message: "The API IS WORKING" });
 });
 
 // register
-app.post("/register", async (req, res) => {
+app.post("/register", async (req, res, next) => {
   try {
     const { email, password, age, name, gender } = req.body;
     const exsitingUser = await User.findOne({ email });
@@ -131,8 +125,8 @@ app.post("/login", async (req, res, next) => {
   }
 });
 
-app.get("/getuserProfile", autnenticateToken, async (req, res) => {
-  const { id } = req.body;
+app.get("/getuserProfile", autnenticateToken, async (req, res, next) => {
+  const id = req.id
   const user = await User.findById(id);
 
   //     if you hide password
@@ -144,41 +138,41 @@ app.get("/refresh_token", async (req, res, next) => {
   const token = req.cookies.refreshToken;
 
   //   res.send(token)
-  if (!token) { 
-        const error = new Error("Token not Found");
-        next(error);
+  if (!token) {
+    const error = new Error("Token not Found");
+    next(error);
   }
 
-  jwt.verify( token, process.env.JWT_REFRESH_SECRET_KEY, async (err, decoded) => {
-      if (err) {
-        const error = new Error("Invalid Token");
-        next(error);
-      }
-
-      const id = decoded.id;
-      const exsitingUser = await User.findById(id);
-      if (!exsitingUser || token !== exsitingUser.refreshToken) {
-        const error = new Error("Invalid Token");
-        next(error);
-      };
-
-      const accesstoken = jwt.sign({ id: exsitingUser._id },process.env.JWT_SECRET_KEY,{ expiresIn: "1hr" });
-  
-      const refreshToken = jwt.sign({ id: exsitingUser._id },process.env.JWT_REFRESH_SECRET_KEY);
-      exsitingUser.refreshToken = refreshToken;
-      await exsitingUser.save();
-      res.cookie("refreshToken", refreshToken, { httpOnly: true, path: "/refresh_token",});
-
-      res.status(200).json({
-        accesstoken,
-        refreshToken,
-        message: "Token Refreshed Successfully!",
-      });
+  jwt.verify(token, process.env.JWT_REFRESH_SECRET_KEY, async (err, decoded) => {
+    if (err) {
+      const error = new Error("Invalid Token");
+      next(error);
     }
+
+    const id = decoded.id;
+    const exsitingUser = await User.findById(id);
+    if (!exsitingUser || token !== exsitingUser.refreshToken) {
+      const error = new Error("Invalid Token");
+      next(error);
+    };
+
+    const accesstoken = jwt.sign({ id: exsitingUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: "1hr" });
+
+    const refreshToken = jwt.sign({ id: exsitingUser._id }, process.env.JWT_REFRESH_SECRET_KEY);
+    exsitingUser.refreshToken = refreshToken;
+    await exsitingUser.save();
+    res.cookie("refreshToken", refreshToken, { httpOnly: true, path: "/refresh_token", });
+
+    res.status(200).json({
+      accesstoken,
+      refreshToken,
+      message: "Token Refreshed Successfully!",
+    });
+  }
   );
 });
 
-// ERROR HANDLING MIDLEARE
+// ERROR HANDLING MIDLEWARE
 app.use((err, req, res, next) => {
   console.log("error middleware called: ", err);
   res.status(500).json({ message: err.message });
